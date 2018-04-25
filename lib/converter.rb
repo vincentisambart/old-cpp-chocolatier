b936a4dd8dabe44de7295e673047ca7b35425eed
Vincent Isambart <vincent.isambart@gmail.com>
2018-04-25 20:52:33 +0900

    Start converting types

--- lib/converter.rb
+++ lib/converter.rb
@@ -12,12 +12,85 @@ class Converter
     raise "Could not find declaration #{usr}"
   end
 
+  def void_type?(type)
+    type[:type_class] == "Builtin" && type[:name] == "Void"
+  end
+
+  def ptr_to_id?(type)
+    return false unless type[:type_class] == "ObjCObjectPointer" && type[:pointee][:type_class] == "ObjCObject"
+    base_type = type[:pointee][:base_type]
+    base_type && base_type[:type_class] == "Builtin" && base_type[:name] == "ObjCId"
+  end
+
+  def rustify_type(type)
+    case type[:type_class]
+    when "Builtin"
+      case type[:name]
+      when "Void"
+        "libc::c_void"
+      when "Char_U", "Char_S"
+        "libc::c_char"
+      when "UChar"
+        "libc::c_uchar"
+      when "UShort"
+        "libc::c_ushort"
+      when "UInt"
+        "libc::c_uint"
+      when "ULong"
+        "libc::c_ulong"
+      when "ULongLong"
+        "libc::c_ulonglong"
+      when "SChar"
+        "libc::c_schar"
+      when "Short"
+        "libc::c_short"
+      when "Int"
+        "libc::c_int"
+      when "Long"
+        "libc::c_long"
+      when "LongLong"
+        "libc::c_longlong"
+      when "Float"
+        "libc::c_float"
+      when "Double"
+        "libc::c_double"
+      else
+        raise "Unknown builtin type #{type.inspect}"
+      end
+    when "Typedef"
+      decl = find_decl(type[:decl_usr])
+      if type[:name] == "BOOL" && decl[:type][:type_class] == "Builtin" && decl[:type][:name] = "SChar"
+        "objc::runtime::BOOL"
+      elsif type[:name] == "instancetype" && ptr_to_id?(decl[:type])
+        "Self"
+      elsif type[:name] == "id" && ptr_to_id?(decl[:type])
+        "id---TODO"
+      elsif type[:name] == "SEL" && decl[:type][:type_class] == "Pointer" && decl[:type][:pointee][:type_class] == "Builtin" && decl[:type][:pointee][:name] = "ObjCSel"
+        "objc::runtime::Sel"
+      elsif type[:name] == "Class" && decl[:type][:type_class] == "ObjCObjectPointer" && decl[:type][:pointee][:type_class] == "ObjCObject" # TODO
+        "Class---TODO"
+# {:is_implicit=>true, :is_referenced=>false, :kind=>"Typedef", :name=>"Class", :type=>{:pointee=>{:base_type=>{:name=>"ObjCClass", :type_class=>"Builtin"}, :type_class=>"ObjCObject"}, :type_class=>"ObjCObjectPointer"}, :usr=>"c:@T@Class"}
+      else
+        "todo"
+      end
+    else
+      "todo"
+    end
+  end
+
   def convert
     raise "Expecting a TranslationUnit declaration" unless @json[:kind] == "TranslationUnit"
+    puts "extern crate objc;"
+    puts "extern crate libc;"
+
+    # A full declaration might come after its first use so make the list of all declarations first.
     @json[:children].each do |decl|
-      next if decl[:is_forward_declaration]
-      @declarations[decl[:usr]] = decl
-      next if decl[:is_implicit]
+      p decl if decl[:is_implicit]
+      @declarations[decl[:usr]] = decl unless decl[:is_forward_declaration]
+    end
+
+    @json[:children].each do |decl|
+      next if decl[:is_forward_declaration] || decl[:is_implicit]
       next unless %w{ObjCInterface ObjCProtocol ObjCCategory}.include?(decl[:kind])
 
       base_traits = []
@@ -44,7 +117,15 @@ class Converter
       end
       decl[:children].each do |child|
         next unless child[:kind] == "ObjCMethod"
-        puts "    fn #{child[:selector].gsub(":", "_")}"
+        method_name = child[:selector].gsub(":", "_")
+        params = child[:params].map {|param| "#{param[:name]}: #{rustify_type(param[:type])}" }
+        params.unshift("&self") if child[:is_instance_method]
+        if void_type?(child[:return_type])
+          puts "    fn #{method_name}(#{params.join(", ")})"
+        else
+          puts "    fn #{method_name}(#{params.join(", ")}) -> #{rustify_type(child[:return_type])}"
+        end
+        # pp child
       end
       puts "}"
       # pp decl
